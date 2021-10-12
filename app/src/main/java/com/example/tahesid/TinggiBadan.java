96314cf82b196903754918b3afb9ffcf183731f5
asadi <asadiiefji@gmail.com>
2021-10-12 15:03:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tahesid/TinggiBadan.java
@@ -0,0 +1,14 @@
+package com.example.tahesid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TinggiBadan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tinggi_badan);
+    }
+}
