c0ff7794f930ccee562d912b9c1a271d530793e7
wildanelek12 <38659848+wildanelek12@users.noreply.github.com>
2021-10-18 10:41:10 +0700

    add_some_fragment

---
+++ app/src/main/java/com/example/tahesid/ui/profil/FragmentProfile.java
@@ -0,0 +1,22 @@
+package com.example.tahesid.ui.profil;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tahesid.R;
+
+public class FragmentProfile extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return root;
+    }
+}
